cac000996df392b51c6703711b73ee78b1d11013
Anassiddiqui-cedcoss <mohdanassiddiqui@cedcoss.com>
2022-08-03 16:40:58 +0530

    Create two buttons previous and next, above a image container element. On click images should be changed to previous one or next one accordingly.

--- jquerytask.js
+++ jquerytask.js
@@ -1,15 +1,9 @@
 
     $(document).ready(function(){
-        var i=0; 
-        var color1 = ["black", "blue", "brown", "green"];
         
-        setInterval(function(){
-            $(".change").css("color",color1[i]); i = (i + 1) % color1.length;},5000);
-           
-
-
-    $("#confirmpass").keyup(function(){
-        var password=document.getElementById("pass").value;
+        
+        $("#confirmpass").keyup(function(){
+            var password=document.getElementById("pass").value;
         var confirmpassword=document.getElementById("confirmpass").value;
         if(password == confirmpassword)
         {
@@ -21,32 +15,35 @@
             document.getElementById("error").innerHTML="**not matched"
         }
     });
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
     $(".btnEven").click(function(){
         $("tr:even").css("background-color","LightGrey")
     });
     $(".btnOdd").click(function(){
         $("tr:odd").css("background-color","DodgerBlue ")
     });
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
     $("#submit").click(function(){
-       first=document.getElementById("fname").value;
-       last=document.getElementById("lname").value;
-       if(first==""){
+        first=document.getElementById("fname").value;
+        last=document.getElementById("lname").value;
+        if(first==""){
            $("#fname").css("background-color","red")
            alert("It cant be blank")
-       }
-       if(last==""){
+        }
+        if(last==""){
            $("#lname").css("background-color","red")
            alert("It cant be blank")
-       }
+        }
     });
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
     $(".add").click(function(){
         clas=document.getElementById("class").value;
         board=document.getElementById("board").value;
         marks=document.getElementById("marks").value;
         division=document.getElementById("divs").value;
         $("body").append("<div><input type='text'placeholder=Enter class><input type='text' placeholder='Enter board'><input type='text' placeholder='Enter Marks'><input type='text'placeholder='Enter Division'><button class='btn'>-</button></div>");
-       })
-      
+    })
+    
     $(".minus").click(function(){
         $(".details").remove();
     })
@@ -54,8 +51,52 @@
         $(this).parent().remove();
     }));
     
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    var i=0; 
+    var color1 = ["black", "blue", "brown", "green"];
     
+    setInterval(function(){
+        $(".change").css("color",color1[i]); i = (i + 1) % color1.length;},5000);
+       
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////   
+ image=["img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg"];
+ var j=2;
+ $("#btnPrev").click(function(){
+    if(j==-1) j=4;
+    if(j>=0){
+       
+        $("img").attr("src",image[j])
+        j--;
+       
+    }
     
+ })
+ $("#btnNext").click(function(){
+    if(j<=4){
+        
+        $("img").attr("src",image[j])
+        j = (j + 1) % image.length;
+    }
+    
+    console.log(image)
+
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
-  
+
+
 
