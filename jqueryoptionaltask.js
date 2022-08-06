3ca98080e6a25c20fa3641ca4ab14251e532d1b9
Anassiddiqui-cedcoss <mohdanassiddiqui@cedcoss.com>
2022-08-06 16:15:17 +0530

    Write jQuery Code to add the index of the list item at the beginning of the list item. 

--- jqueryoptionaltask.js
+++ jqueryoptionaltask.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-    $("li").eq(1).css("background-color", "yellow");
-
-  
-    })
+$( 'li' ).each(function( index, elem ) {
+    $( elem ).prepend( '' + index + ': ' );
+});
