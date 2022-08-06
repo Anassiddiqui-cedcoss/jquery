2f2c2e4b3cc6eb838a37a1fc307da1e3abcd828e
Anassiddiqui-cedcoss <mohdanassiddiqui@cedcoss.com>
2022-08-06 16:47:04 +0530

    Write jQuery Code to change the hyperlink and the text of a existing link.

--- jqueryoptionaltask.js
+++ jqueryoptionaltask.js
@@ -1,3 +1,9 @@
-$( 'li' ).each(function( index, elem ) {
-    $( elem ).prepend( '' + index + ': ' );
+$(document).ready(function(){
+$("#chng").click(function(){
+
+    
+    $("a").attr("href","https://www.google.com");
+    $("a").text("Google")
+    
+});
 });
