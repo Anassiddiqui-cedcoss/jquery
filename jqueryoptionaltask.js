e4b0816ae6aee05f6340fd59ee405af06b6c59d2
Anassiddiqui-cedcoss <mohdanassiddiqui@cedcoss.com>
2022-08-06 15:55:04 +0530

    Write a jQuery Code to get a single element from a list.

--- jqueryoptionaltask.js
+++ jqueryoptionaltask.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-   $append="<h4>Thursday</h4><h5>Friday</h5><h6>saturday</h6>"
-   $("#days").append($append)
+    $("li").eq(1).css("background-color", "yellow");
+
   
     })
